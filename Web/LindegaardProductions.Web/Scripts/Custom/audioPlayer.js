2ddd3f53f34178de191785b63bcac130dc7c3001
André Lange <andre@lait.dk>
2020-09-25 21:20:40 +0200

    Added simple audio player, and setup for aside.

---
+++ Web/LindegaardProductions.Web/Scripts/Custom/audioPlayer.js
@@ -0,0 +1,16 @@
+﻿
+
+const audioPlayer = $(".audio-player");
+const audioPlayerSrc = $("#audio-player__src");
+const audioNumbers = $(".audioNumber");
+if (audioPlayer.length > 0) {
+    audioNumbers.each(function () {
+        $(this).on("click", function (e) {
+            let src = $(this).attr("data-music-src");
+            audioPlayerSrc.attr("src", src)[0];
+            audioPlayer[0].pause();
+            audioPlayer[0].load();
+            audioPlayer[0].play();
+        });
+    });
+}
