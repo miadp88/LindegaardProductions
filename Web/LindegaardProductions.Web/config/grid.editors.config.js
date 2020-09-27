e9fd26b54f0cc107feb470e6b645d4abb03c5f0e
André Lange <andre@lait.dk>
2020-09-27 11:37:40 +0200

    Added headline package and html for article and textpage.

--- Web/LindegaardProductions.Web/config/grid.editors.config.js
+++ Web/LindegaardProductions.Web/config/grid.editors.config.js
@@ -13,28 +13,10 @@
         "icon": "icon-picture"
     },
     {
-        "name": "Macro",
-        "nameTemplate": "{{ value && value.macroAlias ? value.macroAlias : '' }}",
-        "alias": "macro",
-        "view": "macro",
-        "icon": "icon-settings-alt"
-    },
-    {
         "name": "Embed",
         "alias": "embed",
         "view": "embed",
         "icon": "icon-movie-alt"
-    },
-    {
-        "name": "Headline",
-        "nameTemplate": "{{ value }}",
-        "alias": "headline",
-        "view": "textstring",
-        "icon": "icon-coin",
-        "config": {
-            "style": "font-size: 36px; line-height: 45px; font-weight: bold",
-            "markup": "<h1>#value#</h1>"
-        }
     },
     {
         "name": "Quote",
