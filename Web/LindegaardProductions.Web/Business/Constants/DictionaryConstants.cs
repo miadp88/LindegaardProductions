a3f174eee75c8b3be7f45e4cad902211f4ec9a61
André Lange <andre@lait.dk>
2020-09-28 16:34:56 +0200

    Added dictionary for hardcoded values.

--- Web/LindegaardProductions.Web/Business/Constants/DictionaryConstants.cs
+++ Web/LindegaardProductions.Web/Business/Constants/DictionaryConstants.cs
@@ -9,5 +9,25 @@ namespace LindegaardProductions.Web.Business.Constants
     public static class DictionaryConstants
     {
         // add constants to buttons etc.
+        // use the following: @Umbraco.GetDictionaryValue(DictionaryConstants.TitleContact)
+
+        // Title
+        public const string TitleContact = "Title.Contact";
+        public const string TitleNewestNews = "Title.NewestNews";
+        public const string TitleContactMe = "Title.ContactMe";
+        public const string TitleUnsupportedVideo = "Title.UnsupportedVideo";
+        // Forms
+        public const string FormCountry = "Form.Country";
+        public const string FormEmail = "Form.Email";
+        public const string FormMessage = "Form.Message";
+        public const string FormName = "Form.Name";
+        public const string FormSend = "Form.Send";
+        public const string FormSubject = "Form.Subject";
+
+
+        // Buttons
+        public const string ButtonReadMore = "Button.Readmore";
+        public const string ButtonReadMoreNews = "Button.ReadMoreNews";
+
     }
 }
